b03eca3796a8121d03f543632a3bb6de5a513c90
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2017-05-01 00:40:29 +0200

    Rewrite reporter; adds config options, works with newer Puppet
    Tested with puppetserver 2.7.2.

--- lib/puppet/reports/riemann.rb
+++ lib/puppet/reports/riemann.rb
@@ -3,55 +3,62 @@ require 'yaml'
 
 begin
   require 'riemann/client'
-rescue LoadError => e
-  Puppet.info "You need the `riemann-client` gem to use the Riemann report"
+rescue LoadError => ex
+  Puppet.info "You need the `riemann-client` gem to use the Riemann report: #{ex}"
 end
 
+CONFIGFILE = File.join([Puppet.settings[:confdir], 'riemann.yaml'])
+raise(Puppet::ParseError, "Riemann report config file #{configfile} not readable") unless File.exist?(CONFIGFILE)
+
+CONFIG = YAML.load_file(CONFIGFILE)
+
+RIEMANN_SERVER = CONFIG['server'] ||= 'localhost'
+RIEMANN_PORT = CONFIG['port'] ||= 5555
+RIEMANN_PROTOCOL = CONFIG['protocol'] ||= 'tcp'
+RIEMANN_TIMEOUT = CONFIG['timeout'] ||= 60
+PUPPET_FAIL_STATE = CONFIG['fail_state'] ||= 'critical'
+
 Puppet::Reports.register_report(:riemann) do
-
-  configfile = File.join([File.dirname(Puppet.settings[:config]), "riemann.yaml"])
-  raise(Puppet::ParseError, "Riemann report config file #{configfile} not readable") unless File.exist?(configfile)
-  config = YAML.load_file(configfile)
-  RIEMANN_SERVER = config[:riemann_server] ||= 'localhost'
-  RIEMANN_PORT = config[:riemann_port] ||= 5555
-
   desc <<-DESC
   Send metrics to Riemann.
   DESC
 
   def process
-    Puppet.debug "Sending metrics for #{self.host} to Riemann server at #{RIEMANN_SERVER}"
-    @client = Riemann::Client.new(:host => "#{RIEMANN_SERVER}", :port => RIEMANN_PORT)
-    if self.status == 'failed'
-      state = 'critical'
-    else
-      state = 'ok'
-    end
-    event = {
-        :host => self.host,
-        :service => "puppet",
-        :state => state,
-        :description => "Puppet run for #{self.host} #{self.status} at #{Time.now.asctime}",
-        :tags => ["puppet"]
-      }
-      @client << event
-      self.metrics.each { |metric,data|
-        data.values.each { |val|
-          name = "Puppet #{val[1]} #{metric}"
-          if metric == 'time'
-            units = 'seconds'
-          else
-            units = metric
-          end
-          value = val[2]
-          event = {
-            :host    => self.host,
-            :service => name,
-            :metric  => value,
-            :tags    => ["puppet"]
-          }
-          @client << event
+    Puppet.debug "Sending metrics for #{host} to Riemann server at #{RIEMANN_SERVER}"
+    @r = Riemann::Client.new(
+      host: RIEMANN_SERVER,
+      port: RIEMANN_PORT,
+    )
+
+    @client = @r.method RIEMANN_PROTOCOL
+
+    state = if status == 'failed'
+              PUPPET_FAIL_STATE
+            else
+              'ok'
+            end
+
+    @client.call << {
+      host: host,
+      service: 'puppet',
+      state: state,
+      description: "Puppet run for #{host} #{status} at #{Time.now.asctime}",
+      tags: ['puppet', 'status'],
+      timeout: RIEMANN_TIMEOUT,
+    }
+
+    metrics.each { |metric, data|
+      data.values.each { |(key, value)|
+        name = "Puppet #{key} #{metric}"
+
+        @client.call << {
+          host: host,
+          service: name,
+          metric: value,
+          tags: ['puppet', 'metric'],
+          timeout: RIEMANN_TIMEOUT,
         }
       }
+    }
   end
 end
